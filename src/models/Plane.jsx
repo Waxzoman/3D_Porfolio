75e951a9bcf5aa920c9323de21dad537263e70bf
Waxzoman <omar.morsy.yakout@gmail.com>
2023-12-03 13:51:45 +0100

    Added 3d plane to Home

--- src/models/Plane.jsx
+++ src/models/Plane.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 
+
+import planceScene from '../assets/3d/plane.glb'; 
+import { Gltf, useGLTF } from '@react-three/drei';
+
 const Plane = () => {
+  const {scene, animations} = useGLTF(planceScene)  
   return (
     <mesh>
-          
+          <primitive object={scene} />
     </mesh>
   )
 }
