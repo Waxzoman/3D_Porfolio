a3e23c2e97792a555a8975461cfe136fef623295
Waxzoman <omar.morsy.yakout@gmail.com>
2023-12-06 04:05:22 +0100

    created useAlert hook, added Alert component to contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -3,6 +3,8 @@ import emailjs from '@emailjs/browser'
 import { Canvas } from '@react-three/fiber';
 import Fox from '../models/Fox';
 import Loader from '../components/Loader';
+import useAlert from '../hooks/useAlert';
+import Alert from '../components/Alert';
 
 
 const Contact = () => {
@@ -10,6 +12,8 @@ const Contact = () => {
   const [form, setForm] = useState({ name: '', email: '', message: '' });
   const [isLoading, setIsLoading] = useState(false)
   const [currentAnimation, setCurrentAnimation] = useState('idle');
+
+  const { alert, showAlert, hideAlert } = useAlert();
   
   const handleChange = (e) => {
     setForm({ ...form, [e.target.name]: e.target.value });
@@ -32,9 +36,9 @@ const Contact = () => {
       },
       import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY).then(() => {
       setIsLoading(false);
-      // todo: show a success message
-      //todo: hide an alert
+        showAlert({ show: true, text: 'Message sent successfully', type: 'success' });
         setTimeout(() => {
+          hideAlert();
           setCurrentAnimation('idle');
           setForm({ name: '', email: '', message: '' });
          }, [3000]);
@@ -42,7 +46,7 @@ const Contact = () => {
       setIsLoading(false);
       setCurrentAnimation('idle')
       console.log(error);
-      //todo: show error message
+      showAlert({ show: true, text: 'Error! Message was not sent', type: 'danger' });
     })
   };
 
@@ -53,6 +57,8 @@ const Contact = () => {
     <section
       className='relative flex lg:flex-row flex-col max-container'
     >
+      {alert.show && <Alert {...alert} />}
+      
       <div className='flex-1  min-w-[50%] flex flex-col'>
         <h1 className='head-text'>Get in Touch</h1>
 
